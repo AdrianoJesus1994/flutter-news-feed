9d3a00a6cf24c8724eb469a4867fad978feada60
Adriano Jesus <>
2021-04-30 01:04:38 -0300

    feat: initial project

---
+++ lib/pages/home.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text(widget.title),
+        backgroundColor: Theme.of(context).primaryColor,
+        actions: <Widget>[
+          new Padding(
+              child: new Icon(Icons.favorite_border_outlined),
+              padding: const EdgeInsets.only(right: 10))
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text('Teste'),
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
